fe2244b5fef1248e4516745b882a8f18927dc445
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-29 23:07:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(array) # code an argument here
   # Your code here
-  
+  array.collect { |word| word.capitalize + "!" }
 end
 
 def long_planeteer_calls(arrray) # code an argument here
