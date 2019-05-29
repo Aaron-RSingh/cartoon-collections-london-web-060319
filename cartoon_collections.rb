6e05e29a30f15cdcf92e1166c6d69e2f7d5b03da
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-29 23:07:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,17 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
   array.each_with_index { |word, index| puts "#{index+1} #{word}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array) # code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arrray) # code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
